a81f1d1e0a31aec6e8311f172609dd0bf453dae2
timotheo <ccs.timotheoche@gmail.com>
2024-11-05 14:38:22 +0800

    Update: Add background photo and login section photo

--- Ticketing_Project/Form1.Designer.cs
+++ Ticketing_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ticketing_Project
 {
-    partial class Form1
+    partial class form_Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_Login));
+            this.pb_LoginSection = new System.Windows.Forms.PictureBox();
+            this.pb_LoginBackgroundPhoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_LoginSection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_LoginBackgroundPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_LoginSection
+            // 
+            this.pb_LoginSection.Image = global::Ticketing_Project.Properties.Resources.Login_Section;
+            this.pb_LoginSection.Location = new System.Drawing.Point(102, 159);
+            this.pb_LoginSection.Name = "pb_LoginSection";
+            this.pb_LoginSection.Size = new System.Drawing.Size(472, 424);
+            this.pb_LoginSection.TabIndex = 1;
+            this.pb_LoginSection.TabStop = false;
+            // 
+            // pb_LoginBackgroundPhoto
+            // 
+            this.pb_LoginBackgroundPhoto.Image = global::Ticketing_Project.Properties.Resources.Login_Background_Photo;
+            this.pb_LoginBackgroundPhoto.Location = new System.Drawing.Point(695, 12);
+            this.pb_LoginBackgroundPhoto.Name = "pb_LoginBackgroundPhoto";
+            this.pb_LoginBackgroundPhoto.Size = new System.Drawing.Size(663, 729);
+            this.pb_LoginBackgroundPhoto.TabIndex = 0;
+            this.pb_LoginBackgroundPhoto.TabStop = false;
+            // 
+            // form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1370, 755);
+            this.Controls.Add(this.pb_LoginSection);
+            this.Controls.Add(this.pb_LoginBackgroundPhoto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "form_Login";
+            this.Text = "Cloud Express";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_LoginSection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_LoginBackgroundPhoto)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_LoginBackgroundPhoto;
+        private System.Windows.Forms.PictureBox pb_LoginSection;
     }
 }
 
--- Ticketing_Project/Form1.cs
+++ Ticketing_Project/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ticketing_Project
 {
-    public partial class Form1 : Form
+    public partial class form_Login : Form
     {
-        public Form1()
+        public form_Login()
         {
             InitializeComponent();
         }
