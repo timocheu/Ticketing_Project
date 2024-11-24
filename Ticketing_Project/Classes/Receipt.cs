a3120ab94843175d7edd543bd1ae521cdc4cd70b
timotheo <ccs.timotheoche@gmail.com>
2024-11-24 17:05:35 +0800

    Add new class receipt

--- Ticketing_Project/Classes/Receipt.cs
+++ Ticketing_Project/Classes/Receipt.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Ticketing_Project.Classes
 {
-    internal class Receipt
+    public class Receipt
     {
+        public int ReceiptID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Email { get; set; }
+        public int Passengers { get; set; }
+        public string PaymentMethod { get; set; }
+        public string TripType { get; set; }
+        public int Total { get; set; }
     }
 }
